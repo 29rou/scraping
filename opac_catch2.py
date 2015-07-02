0f548d07b427a2a8fd6eab2c902b019cffc5dfdb
29ろう <c171401r@icu.ac.jp>
2015-07-03 04:39:01 +0900

    可読性の向上?

--- opac_catch2.py
+++ opac_catch2.py
@@ -2,14 +2,20 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 
+output_file_dir = './test.text' #出力先
+output_file = open(output_file_dir,'w')
+search_word = '機械学習'
+book_data = [] #書籍情報(Book_dataインスタンス)のリスト
+
 #本のタイトルとリンクを紐付けの為及び汎用性向上のためクラスを作成
 class Book_data():
     instance_count = 0 #インスタンスの数をしめす
-    def __init__(self,data):
+    def __init__(self,data,output):
         self.no = self.next()
         self.name ,self.link = self.extract_data(data)
+        self.write_file(output)
     @classmethod
-    def next(cls):#インスタントのカウント
+    def next(cls):#インスタンスのカウント
         cls.instance_count +=1
         return cls.instance_count
     def extract_data(self,data):
@@ -26,41 +32,37 @@ class Book_data():
         print('Name:', self.name)
         print('Link:', self.link)
     def write_file(self,file):
-        f.write('No:'+str(self.no)+'\n')
-        f.write('Name:'+self.name+'\n')
-        f.write('Link:'+self.link+'\n')
-        f.write('\n')
+        file.write('No:'+str(self.no)+'\n')
+        file.write('Name:'+self.name+'\n')
+        file.write('Link:'+self.link+'\n')
+        file.write('\n')
 
-
-f = open('./test.text','w') #カレントディレクトリに結果を出力する。
-#クローム用のドライバー呼び出し。ドライバーは各々でダウンロードして用意。
-driver = webdriver.Chrome('C:\Program Files (x86)\Google\Chrome\Application\chromedriver.exe')
+#ドライバーの場所、ドライバーは各々でダウンロードして用意
+driver_dir = 'C:\Program Files (x86)\Google\Chrome\Application\chromedriver.exe'
+driver = webdriver.Chrome(driver_dir) #クローム用のドライバー呼び出し。
 driver.get("http://www-lib.icu.ac.jp/")
-driver.find_element_by_name('kywd').send_keys('機械学習')
+driver.find_element_by_name('kywd').send_keys(search_word)
 driver.find_element_by_name('kywd').send_keys(Keys.RETURN)
 driver.close()
 driver.switch_to.window("opac") #検索結果を表示したウインドウに移動。
 html = driver.page_source #HTMLの取得
 
-#データの整形
-#必要のないHTMLコードの上部分を除去
+
+#データの整形:必要のないHTMLコードの上部分を除去
 html_cut_head = html.split('<div id="main_list">',1)
-#必要のないHTMLコードの下部分を除去
+#データの整形:必要のないHTMLコードの下部分を除去
 html_cut_last = html_cut_head[1].split('</div><div class="link_block">',1)
-#本のデータの塊ごとをリストに入れる
+#本のデータを塊ごとをリストに入れる
 data = html_cut_last[0].split('<span class="name">')
 
-book_data = [] #book_dataインスタンスのリスト
-
-#実際にデータをクラスに入れる。
+#本のデータをクラスに入れる。
 for n in range(1,len(data)):
-    book_data.append(Book_data(data[n]))
+    book_data.append(Book_data(data[n],output_file))
 
 driver.close() #ウインドウを閉じる
 
 #データの出力
 for n in range(len(book_data)):
-    book_data[n].write_file(f)
     book_data[n].show_info()
 
-f.close()
+output_file.close() #ファイルを閉じる。
