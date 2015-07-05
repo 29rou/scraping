1030d384c4dc4cf2f4b25a42405508fa32dab6f5
29ろう <c171401r@icu.ac.jp>
2015-07-06 01:00:15 +0900

    Urllib

--- opac_catch3.py
+++ opac_catch3.py
@@ -1,14 +1,9 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-# ブラウザの作成
-from splinter import Browser
-browser = Browser('phantomjs')
-
-# Yahoo!JAPAN のサイトへ移動
-browser.visit('https://opac.icu.ac.jp/opac/opac_search.cgi?smode=1')
-
-# 検索ボックスに「猫」と入力
-browser.find_by_name('kywd').first.fill(u'猫\n')
-
-# 画像検索ボタンをクリック
-#browser.find_by_id('isearch').first.click()
+import urllib.request
+with urllib.request.urlopen('https://opac.icu.ac.jp/opac/opac_search.cgi?smode=1') as response:
+    html = response.read()
+html = str(html)
+print(html.encode('utf8'))
+f = open('./test.html','w+')
+f.write(html)
